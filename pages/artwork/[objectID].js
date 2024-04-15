290f3339275ca95aefd5e6c1e4ebeca2bff11c15
1llen <allenheldhostage@gmail.com>
2024-04-14 20:10:24 -0400

    Removed uneccessary useeffect

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -1,7 +1,6 @@
 import { Row, Col } from "react-bootstrap";
 import ArtworkCardDetail from "../../components/ArtworkCardDetail";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function ArtworkById({ objectID }) {
 	// use router to extract query string
