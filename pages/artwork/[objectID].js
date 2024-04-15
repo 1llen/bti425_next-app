6aae269d0c706dc4d3b79e18e622525c583e5347
1llen <allenheldhostage@gmail.com>
2024-04-14 20:05:41 -0400

    Added useeffect to artwork?

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -1,6 +1,7 @@
 import { Row, Col } from "react-bootstrap";
 import ArtworkCardDetail from "../../components/ArtworkCardDetail";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function ArtworkById({ objectID }) {
 	// use router to extract query string
